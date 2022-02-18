5c508c45eacd9dbff480b9fab1b5d200e5b2e6c4
igavlik <ivangalik963@gmail.com>
2022-02-18 12:10:22 +0100

    remove paggination

--- src/app/ui/shopping-list-table/shopping-list-table.component.ts
+++ src/app/ui/shopping-list-table/shopping-list-table.component.ts
@@ -1,51 +1,17 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
-import {Subscription} from 'rxjs';
-import {ShoppingListTable, ShoppingListTableItem} from './shopping-list-table';
+import {Component,  Input} from '@angular/core';
+import {ShoppingListTable} from './shopping-list-table';
 
 @Component({
   selector: 'hellena-shopping-list-table',
   templateUrl: './shopping-list-table.component.html',
   styleUrls: ['./shopping-list-table.component.css']
 })
-export class ShoppingListTableComponent  implements OnInit, AfterViewInit, OnDestroy {
+export class ShoppingListTableComponent {
 
   @Input()
   table?: ShoppingListTable;
 
-
-  dataSource = new MatTableDataSource<ShoppingListTableItem>();
-  @ViewChild(MatPaginator) paginator: MatPaginator = {} as MatPaginator;
-
-  pageSub: Subscription = {} as Subscription;
-
   constructor() { }
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-
-/* TODO
-  this.pageSub = this.paginator.page.subscribe(el => {
-      this.loadPage.emit({
-        index: el.pageIndex,
-        size: el.pageSize
-      } as LoadPage);
-    });
-
- */
-  }
-
-  ngOnDestroy(): void {
- //   if (this.pageSub) {
- //     this.pageSub.unsubscribe();
- //   }
-  }
 }
 
-export interface LoadPage {
-  index: number;
-  size: number;
-}
