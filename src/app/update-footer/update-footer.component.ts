e8e696637f15665ac22f72be06199d09f61b1e7c
saidulislam <mislam171222@bscse.uiu.ac.bd>
2022-03-09 17:26:08 +0600

    about page add

---
+++ src/app/update-footer/update-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-footer',
+  templateUrl: './update-footer.component.html',
+  styleUrls: ['./update-footer.component.css']
+})
+export class UpdateFooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
