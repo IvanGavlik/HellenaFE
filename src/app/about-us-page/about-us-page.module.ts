638585e745f924d5858e866efff0202c00eb744a
igavlik <ivangalik963@gmail.com>
2022-03-27 15:09:04 +0200

    about us page

--- src/app/about-us-page/about-us-page.module.ts
+++ src/app/about-us-page/about-us-page.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsComponent } from './about-us/about-us.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 
@@ -9,7 +10,8 @@ import { AboutUsComponent } from './about-us/about-us.component';
     AboutUsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   exports: [
     AboutUsComponent
