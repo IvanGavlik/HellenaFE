c90c0fe7f8af236261877848acc14c4c332ca358
igavlik <ivangalik963@gmail.com>
2022-04-09 21:31:40 +0200

    catalogue layout

--- src/app/item-insert/catalogue-display/catalogue-display.component.ts
+++ src/app/item-insert/catalogue-display/catalogue-display.component.ts
@@ -10,19 +10,29 @@ export class CatalogueDisplayComponent implements OnInit, OnDestroy {
   @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement> = {} as ElementRef;
   ctx: CanvasRenderingContext2D | null = null;
   rect = { startX: 0, startY: 0, w : 0, h : 0, };
+  imageObj = new Image();
   drag = false;
   offsetLeft = 0;
   offsetTop = 0;
+
+
+  @ViewChild('canvas2', { static: true }) canvas2: ElementRef<HTMLCanvasElement> = {} as ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
+    this.imageObj.onload = (event) => this.loadImage(event, this.ctx);
+    this.imageObj.src = '../../assets/image/lidl.png';
     this.offsetLeft = this.canvas.nativeElement.offsetLeft;
     this.offsetTop =  this.canvas.nativeElement.offsetTop;
     this.canvas.nativeElement.addEventListener('mousedown', ev => this.mouseDown(ev), false);
     this.canvas.nativeElement.addEventListener('mouseup', ev => this.mouseUp(ev), false);
     this.canvas.nativeElement.addEventListener('mousemove', ev => this.mouseMove(ev), false);
+  }
+
+  loadImage(event: any, ctx: CanvasRenderingContext2D | null): void {
+    ctx?.drawImage(this.imageObj, 0, 0);
   }
 
   ngOnDestroy(): void {}
@@ -41,7 +51,7 @@ export class CatalogueDisplayComponent implements OnInit, OnDestroy {
     if (this.drag) {
 
       this.ctx?.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-//        this.ctx.drawImage(imageObj,imageObj.width/2,imageObj.width/2);
+      this.ctx?.drawImage(this.imageObj, 0, 0);
 
       this.rect.w = (e.pageX - this.offsetLeft) - this.rect.startX;
       this.rect.h = (e.pageY - this.offsetTop) - this.rect.startY;
@@ -51,4 +61,9 @@ export class CatalogueDisplayComponent implements OnInit, OnDestroy {
     }
   }
 
+  doAction(): void {
+    const ctx = this.canvas2.nativeElement.getContext('2d');
+    ctx?.clearRect(0, 0, this.canvas2.nativeElement.width, this.canvas2.nativeElement.height);
+    ctx?.drawImage(this.canvas.nativeElement, this.rect.startX, this.rect.startY, this.rect.w, this.rect.h, 0, 0, this.rect.w, this.rect.h);
+  }
 }
