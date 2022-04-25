2837fdc3d8f72ca3b935a2da9d4d4371c8225942
igavlik <ivangalik963@gmail.com>
2022-04-25 21:23:03 +0200

    shopping cart width

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {MatDialog, MatDialogConfig} from '@angular/material/dialog';
 import {Router} from '@angular/router';
 import {ShoppingListComponent} from '../../shopping-list/shopping-list/shopping-list.component';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Component({
   selector: 'hellena-navbar',
@@ -15,7 +16,7 @@ export class NavbarComponent implements OnInit {
 
   isOpened = false;
 
-  constructor(private router: Router, private dialog: MatDialog) { }
+  constructor(private router: Router, private dialog: MatDialog, private deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
   }
@@ -25,7 +26,11 @@ export class NavbarComponent implements OnInit {
       return;
     }
     const config = {} as MatDialogConfig;
-    config.width = '30%';
+    if (this.deviceService.isDesktop()) {
+      config.width = '30%';
+    } else {
+      config.width = '60%';
+    }
     config.height = '100%';
     const dialog = this.dialog.open(ShoppingListComponent, config);
     dialog.updatePosition({ top: '100px', right: '0px' }  );
