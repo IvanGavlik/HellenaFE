d95e9d81999edb35643a0793911ac474ea9ab5ab
ivangavlik963 <ivangavlik963@gmail.com>
2022-07-24 16:54:31 +0200

    delete ded code item module

--- src/app/front-page/front-page.module.ts
+++ src/app/front-page/front-page.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrontPageComponent } from './front-page/front-page.component';
-import {ItemModule} from '../item/item.module';
 import {UiModule} from '../ui/ui.module';
 import {MatCardModule} from '@angular/material/card';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
-
-
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { MobAppPromotionDialogComponent } from './mob-app-promotion-dialog/mob-app-promotion-dialog.component';
 import { FrontPageDesktopComponent } from './front-page-desktop/front-page-desktop.component';
 import { FrontPageMobileComponent } from './front-page-mobile/front-page-mobile.component';
 import { DailyDealComponent } from './front-page-desktop/daily-deal/daily-deal.component';
 import {CloudinaryModule} from '@cloudinary/ng';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -26,7 +24,7 @@ import {CloudinaryModule} from '@cloudinary/ng';
   ],
     imports: [
         CommonModule,
-        ItemModule,
+        HttpClientModule,
         CarouselModule,
         UiModule,
         MatCardModule,
