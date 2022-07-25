85f47a21ea6c558ffa0cb32e0ab65671febf429b
ivangavlik963 <ivangavlik963@gmail.com>
2022-07-25 23:43:20 +0200

    junit tests - delete existing so that I can start clear

--- src/app/local-storage/native-window.spec.ts
+++
@@ -1,7 +0,0 @@
-import { NativeWindow } from './native-window';
-
-describe('NativeWindow', () => {
-  it('should create an instance', () => {
-    expect(new NativeWindow()).toBeTruthy();
-  });
-});
