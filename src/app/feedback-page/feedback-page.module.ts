7f5dea80abd5f8062761d197bea47048b8696e77
ivangavlik963 <ivangavlik963@gmail.com>
2022-07-28 23:32:41 +0200

    feedback ui

--- src/app/feedback-page/feedback-page.module.ts
+++ src/app/feedback-page/feedback-page.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedbackDialogComponent } from './feedback-dialog/feedback-dialog.component';
-
-
+import {MatSliderModule} from '@angular/material/slider';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     FeedbackDialogComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule,
+    MatInputModule,
+    MatCardModule,
+      MatButtonModule,
+      MatFormFieldModule
   ]
 })
 export class FeedbackPageModule { }
