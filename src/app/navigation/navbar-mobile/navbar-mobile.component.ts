f994bec33b309d79342583ce2411dd6bb62f10a1
ivangavlik963 <ivangavlik963@gmail.com>
2022-08-01 22:02:45 +0200

    navigation add search name input

--- src/app/navigation/navbar-mobile/navbar-mobile.component.ts
+++ src/app/navigation/navbar-mobile/navbar-mobile.component.ts
@@ -4,6 +4,10 @@ import {FeedbackDialogComponent} from '../../feedback-page/feedback-dialog/feedb
 import {LocalStorageService} from '../../local-storage/local-storage.service';
 import {MatDialogRef} from '@angular/material/dialog/dialog-ref';
 import {Subscription} from 'rxjs';
+import {FormControl} from '@angular/forms';
+import {SearchItem} from '../../search-page/search-item';
+import {SearchUIService} from '../../search-page/search-ui.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -12,7 +16,29 @@ import {Subscription} from 'rxjs';
   styleUrls: ['./navbar-mobile.component.css']
 })
 export class NavbarMobileComponent  {
-  constructor() {}
+  search = new FormControl('');
+  searchItem: SearchItem = {} as SearchItem;
+
+  constructor(public searchUI: SearchUIService, private router: Router) {}
+
+  handleNavigationClickSearch(): void {
+    this.handleNavigationClickSearchUtil(this.search.value);
+  }
+
+  handleNavigationClickSearchUtil(value: string | null): void {
+    if (value == null) {
+      this.searchItem.name = undefined;
+    } else {
+      this.searchItem.name = value;
+    }
+    if (this.router.url.includes('/search')) {
+      this.searchUI.searchStart({item: this.searchItem, firstPage: true} );
+    } else {
+      this.router.navigateByUrl('/search', {
+        state: this.searchItem,
+      });
+    }
+  }
 }
 
 
